d6bed5bfb9f5402f9e582d47a305c11d5d29e213
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-17 12:27:45 +0530

    Swamy/15nov work (#4)
    * Update README.md
    
    * Update Palindrome.java
    
    * Update README.md

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ I am learning Algorithms from different Video Courses, Books, and Websites.
 
 ## Running the Project
 
+### Current Working Directory
+
+```text
+D:\TSA-Structura\learn-ds-algorithms>
+```
+
 ### Build the Project: You can build the project using Gradle by running
 
 ```bash
@@ -42,15 +48,15 @@ learn-algorithms/
 │   ├── main/
 │   │   └── java/
 │   │       └── com/
-│   │           └── tsa/
-│   │               └── algorithm/
+│   │           └── ldsa/
+│   │               └── Palindrome2p/
 │   │                   ├── Palindrome.java
 │   │                   └── PalindromeChecker.java
 │   └── test/
 │       └── java/
 │           └── com/
-│               └── tsa/
-│                   └── algorithm/
+│               └── ldsa/
+│                   └── Palindrome2p/
 │                       └── PalindromeTest.java
 ├── .gitignore           # Files or directories to ignore in Git (e.g., .class files, IDE settings)
 ├── LICENSE              # License for your code
--- src/main/java/com/ldsa/Palindrome2p/Palindrome.java
+++ src/main/java/com/ldsa/Palindrome2p/Palindrome.java
@@ -8,13 +8,26 @@ public class Palindrome {
                 "racecar",
                 "hello",
                 "Madam, in Eden, I'm Adam",
-                "12321"
+                "12321",
+                "RACEACAR",
+                "A",
+                "ABCDEFGFEDCBA",
+                "ABC",
+                "ABCBA",
+                "ABBA",
+                "RACEACAR"
         };
 
-        // Check each test case
-        for (String testString : testStrings) {
-            boolean result = PalindromeChecker.isPalindrome(testString);
-            System.out.println("\"" + testString + "\" is palindrome: " + result);
+        // Check each test case with detailed information
+        for (int k = 0; k < testStrings.length; k++) {
+            System.out.println("Test Case #" + (k + 1));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+            System.out.println("The input string is \"" + testStrings[k] + "\" and the length of the string is "
+                    + testStrings[k].length() + ".");
+            System.out.println("\nIs it a palindrome?..... " + PalindromeChecker.isPalindrome(testStrings[k]));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+            System.out.println(); // Blank line between test cases
         }
+
     }
 }
